2f855d57795dac8d4bde899a5f4b3561578da878
JamesP-GitH <james.pagliaccio@gmail.com>
2025-05-09 12:40:10 +1000

    answers Q1

--- JSFundamentals/fundamentals.js
+++ JSFundamentals/fundamentals.js
@@ -1,17 +1,19 @@
-"" + 1 + 0;
-"" - 1 + 0;
-true + false;
-!true;
-6 / "3";
-"2" * "3";
-4 + 5 + "px";
-"$" + 4 + 5;
-"4" - 2;
-"4px" - 2;
-" -9 " + 5;
-" -9 " - 5;
-null + 1;
-undefined + 1;
-undefined == null;
-undefined === null;
-" \t \n" - 2;
+// === Question 1 ===
+
+"" + 1 + 0; // "10"
+"" - 1 + 0; // -1
+true + false; // 1
+!true; // false
+6 / "3"; // 2
+"2" * "3"; // 6
+4 + 5 + "px"; // "9px"
+"$" + 4 + 5; // "$45"
+"4" - 2; // 2
+"4px" - 2; // Not a number (NAN)
+" -9 " + 5; // " -9 5"
+" -9 " - 5; // -14
+null + 1; // 1
+undefined + 1; // Not a number (NAN)
+undefined == null; // true
+undefined === null; // false
+" \t \n" - 2; // -2
