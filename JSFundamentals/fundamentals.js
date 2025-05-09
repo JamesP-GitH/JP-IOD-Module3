2c200c0e0ff66c5d8d54ed2d3940d5d60c840e9e
JamesP-GitH <james.pagliaccio@gmail.com>
2025-05-09 12:42:06 +1000

    Q2 added

--- JSFundamentals/fundamentals.js
+++ JSFundamentals/fundamentals.js
@@ -16,6 +16,18 @@ null + 1; // 1
 undefined + 1; // Not a number (NAN)
 undefined == null; // true
 undefined === null; // false
-" \t \n" - 2; // -2
+" \t \n" - 2; // -2 (\t \n is essentially 0, notation for tab and newline, meaning "\t \n" is empty space)
 
 console.log(" \t \n" - 2);
+
+// === Question 2 ===
+let three = "3";
+let four = "4";
+let thirty = "30";
+//what is the value of the following expressions?
+let addition = three + four;
+let multiplication = three * four;
+let division = three / four;
+let subtraction = three - four;
+let lessThan1 = three < four;
+let lessThan2 = thirty < four;
