377af124906cc28c8f0c4b1733ade6d4c9a4e897
JamesP-GitH <james.pagliaccio@gmail.com>
2025-05-09 14:22:06 +1000

    Answered Q6 added Q7

--- JSFundamentals/fundamentals.js
+++ JSFundamentals/fundamentals.js
@@ -97,13 +97,40 @@ const rugen = {
 };
 const inigo = {
     firstName: "Inigo",
+    lastName: "Montoya",
     greeting(person) {
-        let greeting = `Hello ${person.name}, my name is ${this.firstName}. `;
+        let greeting = `Hello ${person.name}, my name is ${this.firstName} ${this.lastName}. `;
         console.log(greeting + this.getCatchPhrase(person));
     },
-    getCatchPhrase(person) {
-        return "Nice to meet you.";
-    },
+    getCatchPhrase: (person) => (person.numFingers > 5 ? "You killed my father. Prepare to die." : "Nice to meet you."),
 };
 inigo.greeting(westley);
 inigo.greeting(rugen);
+
+// === Question 7 === The following object represents a basketball game and keeps track of the score as the
+// game progresses.
+// a) Modify each of the methods so that they can be ‘chained’ together and the last line of
+// the example code works
+// b) Add a new method to print the full time final score
+// c) Add a new object property to keep track of the number of fouls and a method to
+// increment it, similar but separate to the score. Include the foul count in the half time and
+// full time console messages
+// d) Test your object by chaining all the method calls together in different combinations.
+
+const basketballGame = {
+    score: 0,
+    freeThrow() {
+        this.score++;
+    },
+    basket() {
+        this.score += 2;
+    },
+    threePointer() {
+        this.score += 3;
+    },
+    halfTime() {
+        console.log("Halftime score is " + this.score);
+    },
+};
+//modify each of the above object methods to enable function chaining as below:
+basketballGame.basket().freeThrow().freeThrow().basket().threePointer().halfTime();
