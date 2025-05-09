0953333978e232f9c58c1559395be468b8ac30de
JamesP-GitH <james.pagliaccio@gmail.com>
2025-05-09 13:00:06 +1000

    fixed comments Q4

--- JSFundamentals/fundamentals.js
+++ JSFundamentals/fundamentals.js
@@ -44,11 +44,21 @@ if (1) console.log("positive is true"); // Will print, all non zero numbers incl
 
 // === Question 4 === Rewrite this if using the ternary/conditional operator '?'. Test it with different values for a
 // and b. What does the ‘+=’ do?
-let a = 2,
+
+/* Example given
+let a = 2, 
     b = 3;
 let result = `${a} + ${b} is `;
 if (a + b < 10) {
-    result += "less than 10";
+    result += "less than 10"; 
 } else {
     result += "greater than 10";
 }
+*/
+
+let a = 4,
+    b = 8;
+let result = `${a} + ${b} is `;
+result += a + b < 10 ? "less than 10" : "greater than 10";
+// ^ "+=" adds the string onto the end of the result string, and assigns 'result' the new value of the two strings together.
+console.log(result);
