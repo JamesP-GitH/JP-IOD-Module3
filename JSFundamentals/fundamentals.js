e89d6cbc690905cf7dda8282a5b1a4349383d917
JamesP-GitH <james.pagliaccio@gmail.com>
2025-05-09 12:47:50 +1000

    Q3 added

--- JSFundamentals/fundamentals.js
+++ JSFundamentals/fundamentals.js
@@ -1,4 +1,4 @@
-// === Question 1 ===
+// === Question 1 === What are the results of these expressions? (answer first, then use console.log() to check)
 
 "" + 1 + 0; // "10"
 "" - 1 + 0; // -1
@@ -20,7 +20,8 @@ undefined === null; // false
 
 console.log(" \t \n" - 2);
 
-// === Question 2 ===
+// === Question 2 === Which of the below are not giving the right answer? Why are they not correct? How can we
+// fix them?
 
 let three = "3"; // <- these are all strings
 let four = "4";
@@ -32,3 +33,11 @@ let division = three / four; // 0.75 (implicit converted to numbers by /)
 let subtraction = three - four; // -1 (implicit converted to numbers by -)
 let lessThan1 = three < four; // true (implicit converted to numbers by <)
 let lessThan2 = thirty < four; // false (implicit converted to numbers by <)
+
+// === Question 3 === Which of the following console.log messages will print? Why?
+
+if (0) console.log("#1 zero is true");
+if ("0") console.log("#2 zero is true");
+if (null) console.log("null is true");
+if (-1) console.log("negative is true");
+if (1) console.log("positive is true");
