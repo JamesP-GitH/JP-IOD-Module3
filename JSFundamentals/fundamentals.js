94af7aba96b608599ef7f45d0e57acf25a74632a
JamesP-GitH <james.pagliaccio@gmail.com>
2025-05-09 14:34:38 +1000

    added Q8

--- JSFundamentals/fundamentals.js
+++ JSFundamentals/fundamentals.js
@@ -148,3 +148,18 @@ const basketballGame = {
 //basketballGame.basket().freeThrow().freeThrow().basket().threePointer().halfTime(); // <- original
 //basketballGame.basket().freeThrow().foul().threePointer().halfTime().basket().foul().fullTime(); // <- Test1
 basketballGame.freeThrow().freeThrow().foul().foul().threePointer().halfTime().foul().foul().basket().fullTime(); // <- Test2
+
+// === Question 8 === The object below represents a single city.
+// a) Write a function that takes an object as an argument and uses a for...in loop to access
+// and print to the console each of those object properties and their values. Test it using
+// the sydney object below.
+// b) Create a new object for a different city with different properties and call your function
+// again with the new object.
+
+const sydney = {
+    name: "Sydney",
+    population: 5_121_000,
+    state: "NSW",
+    founded: "26 January 1788",
+    timezone: "Australia/Sydney",
+};
